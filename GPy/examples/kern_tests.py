abea31ffdc8abc33909d7f44590a067767f1ed12
Ricardo <rick70x7@gmail.com>
2013-02-21 00:47:31 +0000

    changes in coreg_kern

--- GPy/examples/kern_tests.py
+++ GPy/examples/kern_tests.py
@@ -18,14 +18,30 @@ _X = np.arange(1,N*D +1).reshape(N,D)
 I_ = np.array([0,0,1,1,1,2,2])[:,None]
 X = np.hstack([I_,_X])
 
-base =  GPy.kern.rbf(2)
-#base = GPy.kern.white(2)
+base1 =  GPy.kern.rbf(2)
+base2 = GPy.kern.white(2)
 #base = GPy.kern.linear(2)
 
-print "B matrix"
-print kernel.parts[0].B
-kernel = GPy.kern.icm(base,3,0)
+print base1.K(_X)
+print base2.K(_X)
+print (base1+base2).K(_X)
+#print base1.add(base2).K(_X) - (base1+base2).K(_X)
 
-#print kernel.K(X) - base.K(_X)
-base.K(_X)
-print kernel.K(X)
+kernel1 = GPy.kern.icm(base1,3,0)
+kernel2 = GPy.kern.icm(base2,3,0)
+
+print "B matrix"
+#print kernel1.parts[0].B
+
+print kernel1.K(X)# - base1.K(_X)
+
+#kernel3 = kernel1.add(kernel2)
+print 'kernel 1'
+print kernel1.K(X)
+print 'kernel 2'
+print kernel2.K(X)
+
+kernel3 = kernel1 + kernel2
+print 'kernel3'
+print kernel3.K(X)
+
--- GPy/kern/coreg_kern.py
+++ GPy/kern/coreg_kern.py
@@ -38,6 +38,36 @@ class coreg_kern(kern):
         X = X[:,_range]
         return X,X_i
 
+    def __add__(self,other):
+        assert self.D == other.D
+        #N = 7
+        #D = 2
+        #R = 3
+        #M = 5
+        #_X = np.arange(1,N*D +1).reshape(N,D)
+        #I_ = np.array([0,0,1,1,1,2,2])[:,None]
+        #X = np.hstack([I_,_X])
+        newkern =  coreg_kern(self.D,self.parts+other.parts, self.input_slices + other.input_slices,self.index)
+        #transfer constraints:
+        newkern.constrained_positive_indices = np.hstack((self.constrained_positive_indices, self.Nparam + other.constrained_positive_indices))
+        newkern.constrained_negative_indices = np.hstack((self.constrained_negative_indices, self.Nparam + other.constrained_negative_indices))
+        newkern.constrained_bounded_indices = self.constrained_bounded_indices + [self.Nparam + x for x in other.constrained_bounded_indices]
+        newkern.constrained_bounded_lowers = self.constrained_bounded_lowers + other.constrained_bounded_lowers
+        newkern.constrained_bounded_uppers = self.constrained_bounded_uppers + other.constrained_bounded_uppers
+        newkern.constrained_fixed_indices = self.constrained_fixed_indices + [self.Nparam + x for x in other.constrained_fixed_indices]
+        newkern.constrained_fixed_values = self.constrained_fixed_values + other.constrained_fixed_values
+        newkern.tied_indices = self.tied_indices + [self.Nparam + x for x in other.tied_indices]
+        return newkern
+
+    def add(self,other):
+        """
+        Add another kernel to this one. Both kernels are defined on the same _space_
+        :param other: the other kernel to be added
+        :type other: GPy.kern
+        """
+        return self + other
+
+
     def K(self,X,X2=None,slices1=None,slices2=None):
         assert X.shape[1]==self.D + 1
         slices1, slices2 = self._process_slices(slices1,slices2)
