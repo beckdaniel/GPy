c468820e0d6b34bbb302b5bbc1a59b9e4ea7c5c5
beckdaniel <daniel.e.beck@gmail.com>
2016-01-15 15:26:56 +0000

    fixing tests and changing code

--- GPy/kern/src/string.py
+++ GPy/kern/src/string.py
@@ -9,23 +9,29 @@ class StringKernel(Kern):
     def __init__(self, name='sk'):
         super(StringKernel, self).__init__(1, 1, name)
 
-    def calc_k(self, s1, s2):
+    def calc_k(self, s1, s2, decay=1.0):
         """
         Do the actual kernel calculation
         """
         n = len(s1)
         m = len(s2)
-        dp = [[1.0] * (m + 1)] * (n + 1)
+        dp = [[decay] * (m + 1)] * (n + 1)
         p = [0.0] * (m + 1)
         for i in xrange(1, n + 1):
-            last = 0
+            #last = 0
             p[0] = 0
             for k in xrange(1, m + 1):
-                p[k] = p[last]
                 if s2[k - 1] == s1[i - 1]:
-                    p[k] = p[last] + dp[i - 1][k - 1]
-                    last = k
+                    sim = 1
+                else:
+                    sim = 0
+                p[k] = p[k - 1] + (dp[i - 1][k - 1] * sim * decay)
+                #p[k] = p[last]
+                #if s2[k - 1] == s1[i - 1]:
+                #    p[k] = p[last] + (dp[i - 1][k - 1] * sim * decay)
+                #    last = k
             for k in xrange(1, m + 1):
                 dp[i][k] = dp[i - 1][k] + p[k]
+            #print dp
         return dp[n][m]
         
--- GPy/testing/string_kernel_tests.py
+++ GPy/testing/string_kernel_tests.py
@@ -12,13 +12,27 @@ class StringKernelTests(unittest.TestCase):
         s1 = 'aab'
         s2 = 'aabb'
         k = GPy.kern.StringKernel()
-        self.assertTrue(k.calc_k(s1, s2), 9)
+        result = k.calc_k(s1, s2)
+        #print result
+        self.assertEquals(result, 18)
 
     def test_sk_2(self):
         s1 = 'gatta'
         s2 = 'cata'
         k = GPy.kern.StringKernel()
-        self.assertTrue(k.calc_k(s1, s2), 14)
+        self.assertEquals(k.calc_k(s1, s2), 14)
+
+    def test_sk_3(self):
+        s1 = 'gatta'
+        s2 = 'cata'
+        k = GPy.kern.StringKernel()
+        self.assertEquals(k.calc_k(s1, s2), k.calc_k(s2, s1))
+
+    def test_sk_4(self):
+        s1 = 'gatta'
+        s2 = 'cata'
+        k = GPy.kern.StringKernel()
+        self.assertEquals(k.calc_k(s1, s2, decay=0.5), 2.75)
 
 
 if __name__ == "__main__":
